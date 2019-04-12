c48096b73b269c3e03dc08525adda3d8868c485d
Jian_Shi <shijianhzchina@gmail.com>
2019-04-13 02:27:26 +1000

    Auxiliary Classes

---
+++ ListNode.java
@@ -0,0 +1,22 @@
+package sj.leetcode;
+
+/**
+ * @author: Jian Shi
+ * @email: shijianhzchina@gmail.com
+ * @date: 12/20/2018 17:20
+ */
+
+public class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+
+    public void setNext(ListNode next){
+        this.next = next;
+    }
+
+}
