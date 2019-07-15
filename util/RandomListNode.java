b1baf5beba879f10d4ad2a0ae616677d90505d91
Jian_Shi <shijianhzchina@gmail.com>
2019-07-16 01:45:27 +1000

    Update Auxiliary Class

---
+++ util/RandomListNode.java
@@ -0,0 +1,17 @@
+package sj.util;
+
+/**
+ * @author: Jian Shi
+ * @email: shijianhzchina@gmail.com
+ * @date: 16/07/2019 1:28 AM
+ */
+
+public class RandomListNode {
+    int label;
+    RandomListNode next = null;
+    RandomListNode random = null;
+
+    RandomListNode(int label) {
+        this.label = label;
+    }
+}
